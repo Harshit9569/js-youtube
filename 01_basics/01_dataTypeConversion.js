2ece2ca4bf1b1af381707babed285554293fa311
Harshit9569 <harshit20310@gmail.com>
2025-07-25 04:58:17 +0000

    completed

--- 01_basics/01_dataTypeConversion.js
+++ 01_basics/01_dataTypeConversion.js
@@ -24,3 +24,51 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 let stringNmuber = String(someNmuber)
  //33 =>"33" type string
+
+ //******operations ****/
+  let value = 3;
+  let negValue = -value //-3
+  //console.log(negValue)
+
+  console.log(2+2)
+  console.log(2-2)
+  console.log(2*2)
+  console.log(2**2)
+  console.log(2/2)
+  console.log(2%2)
+
+  let str1 = "hello"
+  let str2 = "_hitesh"
+  let str3 = str1 + str2
+  console.log(str3)//hello_hitesh
+
+  console.log("1" + 2) //12
+  console.log(1 + "2") //12
+  console.log("1" + 2 + 2) //122
+  console.log(1 + 2 + "2") //32
+
+  console.log(true)//true
+  console.log(+true) // 1
+//console.log(true+) // error
+  console.log(+"");
+
+  let num1,num2,num3
+  num1 = num2 = num3 = 2 + 2
+
+  let gameCounter = 100
+  gameCounter++
+  console.log(gameCounter) // 101
+
+  let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+

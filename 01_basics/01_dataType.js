bc5057b910f5a58eddd50c64c4b3386f08e07421
Harshit9569 <harshit20310@gmail.com>
2025-07-25 04:22:21 +0000

    completed

---
+++ 01_basics/01_dataType.js
@@ -0,0 +1,25 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3 + 3)//we are using node.js not browser
+
+//code readibilty should be high
+
+let name = "hv"
+let age = 18
+let isLoggedIn = false
+let state; //undefined data type
+
+
+//approx range of number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+// null =>standalone value
+// undefined => means abhi vlaue nhi assign hui h
+//symbol => use for uniqueness
+
+// object
+
+console.log(typeof "hitesh")
+console.log(typeof null) //it is an object in js(means type is object)
+console.log(typeof undefined)//type is undefined

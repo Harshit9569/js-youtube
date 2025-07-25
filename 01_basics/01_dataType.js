71fafb169bd8cd04d463288be25c58cb59d3844c
Harshit9569 <harshit20310@gmail.com>
2025-07-25 05:27:29 +0000

    complete most part of dataType

--- 01_basics/01_dataType.js
+++ 01_basics/01_dataType.js
@@ -23,3 +23,39 @@ let state; //undefined data type
 console.log(typeof "hitesh")
 console.log(typeof null) //it is an object in js(means type is object)
 console.log(typeof undefined)//type is undefined
+
+/* Primitive(call by value)
+
+
+7 types; string ,Nmuber ,Boolean ,Null ,Undefined , Symbol,BigInt
+
+*/
+
+
+ 
+//javascript is a dynamically typed language
+ const score = 100
+ const scoreValue = 100.3
+ const isLogged = false
+
+ const id = Symbol('123')
+ const anotherId = Symbol('123')
+ console.log(id === anotherId)
+ const bigNumber = 346543576654356754n //now this will store in BigInt
+
+ /*
+Reference (non primitve )
+
+// Array,Objects,Function(data type of all are function for function is is called function object )
+
+*/
+ const heros = ["shaktiman", "ironman", "naagraj"]
+let myObj={
+    name:"hv",
+    age:18,
+}
+
+const myFunction = function(){
+
+}
+

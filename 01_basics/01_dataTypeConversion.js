87ad467618447832ccc04ecb705ce702f2c9de77
Harshit9569 <harshit20310@gmail.com>
2025-07-25 04:38:41 +0000

    conversion of data types

---
+++ 01_basics/01_dataTypeConversion.js
@@ -0,0 +1,26 @@
+let age = "33"
+
+console.log( typeof age)
+console.log(typeof(age))
+
+let valueInNumber = Number(age)
+console.log(typeof valueInNmuber);
+console.log(valueInNumber);
+//note conversion of null  to number will give  answer 0;
+//is suppose age="33ab" and we are converting it into number then output will be nan and type is number
+
+//"33" =>33
+//"33ab" =>>NaN
+//true =>1; FALSE =>0
+
+let isLoggedIn = 1
+let booleanIsLogged = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 =>true; 0=>false
+//"" =>false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNmuber = String(someNmuber)
+ //33 =>"33" type string
